87917ed726dd87001716f528a83c3529473c1d5d
Egor-Goloshchapov <141495264+Egor-Goloshchapov@users.noreply.github.com>
2024-12-27 23:18:18 +0200

    did hw

--- hw19.js
+++ hw19.js
@@ -19,3 +19,24 @@ const textSpan = document.querySelector('#text');
 fontSizeControl.addEventListener('input', (event) => {
   textSpan.style.fontSize = `${event.target.value}px`;
 });
+
+
+
+const page = document.querySelector("body");
+const backdrop = document.querySelector(`.js-backdrop`);
+const openBtn = document.querySelector(`[data-action="open-modal"]`);
+const closeBtn = document.querySelector(`[data-action="close-modal"]`);
+
+openBtn.addEventListener("click", toogleClass);
+closeBtn.addEventListener("click", toogleClass);
+backdrop.addEventListener("click", toogleClass);
+
+function toogleClass(event) {
+  if (
+    event.currentTarget === backdrop &&
+    event.target !== event.currentTarget
+  ) {
+    return;
+  }
+  page.classList.toggle("show-modal");
+}
